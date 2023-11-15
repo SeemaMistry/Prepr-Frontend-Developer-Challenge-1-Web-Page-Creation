44a46752bc5c352a97a2f78f5cb33c5b190e9f6c
SeemaMistry <seema.m.1996@gmail.com>
2023-11-14 23:04:36 -0500

    feat: add top menu for labs, challenges, resources, projects selection to the center

--- prepr-homepage/src/components/CoursesSection.js
+++ prepr-homepage/src/components/CoursesSection.js
@@ -1,16 +1,20 @@
 import React from 'react'
 
 const CoursesSection = () => {
+    // TODO: conditionally add btn-primary class to clicked buttons using useState
   return (
     <div className='container'>
-        <div>
-            Buttons Menu
+        <div className='text-center'>
+            <button className='btn btn-lg btn-primary'>Labs</button>
+            <button className='btn btn-lg'>Challenges</button>
+            <button className='btn btn-lg'>Resources</button>
+            <button className='btn btn-lg'>Projects</button>
         </div>
         <div className='row'>
-            <div className="col-2 mx-5 p-10 ">
+            <div className="col-2 mx-5 p-10">
                 Conditionally rendered Side Menus 
             </div>
-            <div className="col-9 ">
+            <div className="col-9">
                 <div>Search Bar</div>
                 <div>Conditionally rendered CourseCards</div>
             </div>

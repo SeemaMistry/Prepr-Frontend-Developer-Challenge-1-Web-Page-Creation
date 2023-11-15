6136831ba09a60187473bce6b635d5b1dc4fe68b
SeemaMistry <seema.m.1996@gmail.com>
2023-11-14 21:44:29 -0500

    feat: create HeroSection component

---
+++ prepr-homepage/src/components/HeroSection.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <div className='bg-primary text-center'>
+        <h1 className='p-2 mt-10'>Hero Section</h1>
+    </div>
+  )
+}
+
+export default HeroSection

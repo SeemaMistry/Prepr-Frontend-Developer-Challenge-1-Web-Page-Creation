0216cd2e41578b3032777143424263b04836d470
SeemaMistry <seema.m.1996@gmail.com>
2023-11-14 20:33:12 -0500

    feat: create basic navbar

--- prepr-homepage/src/App.js
+++ prepr-homepage/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world!</h1>
-    </div>
+    <>
+    <Navbar />
+    </>
   );
 }
 
---
+++ prepr-homepage/src/components/Navbar.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import logo from '../assets/preprLogo.png'
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+            <a className="navbar-brand" href="#"><img src={logo} alt='Prepr Logo'/></a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                    <a className="nav-link " aria-current="page" href="/">Explore</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="/">Dashboard</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="/">My Challenges</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="/">My Labs</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="/">Career Explorer</a>
+                </li>
+
+                
+            </ul>
+            <img><i class="bi bi-chat-left-fill"></i></img>
+            <form className="d-flex" role="search">
+                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                <button className="btn btn-outline-success" type="submit">Search</button>
+            </form>
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar

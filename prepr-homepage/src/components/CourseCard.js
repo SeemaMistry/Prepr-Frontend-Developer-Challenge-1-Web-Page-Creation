fd8f98d3f12ce00044ebf7efba7983813e9ec2fb
SeemaMistry <seema.m.1996@gmail.com>
2023-11-15 11:29:53 -0500

    fix: update styling and attributes from vanilla html to React JSX syntax

--- prepr-homepage/src/components/CourseCard.js
+++ prepr-homepage/src/components/CourseCard.js
@@ -1,11 +1,33 @@
 import React from 'react'
+import courseImg from '../assets/preprLogo.png'
 
 const CourseCard = ({course}) => {
+    // Course Object values
+    // id: 4,
+    // host: 'youth',
+    // category: 'labs',
+    // title: 'UX Feedback Lab',
+    // description: 'Welcome to the CARE Lab, a space to collaborate and communicate ideas, resources, best practices, and awareness to help manage stress, relieve anxiety, and carry our collective burdens together. Participants are invited to share stories and resources while creating a safe space for everyone to share and learn from each other. Contributors are invited to host events and webinars that can be made ac...',
+    // tags: ['communication', 'mindfullness', 'writing']
+
   return (
-    <div>
-        <h1>{course.title}</h1>
-        <p>{course.description}</p>
+    <>
+    <div className="card mb-3" style={{maxWidth: '100%'}}>
+        <div className="row g-0">
+            <div className="col-md-4">
+                <img src={courseImg} className="img-fluid rounded-start" alt="..."/>
+            </div>
+            <div className="col-md-8">
+                <div className="card-body">
+                    <h5 className="card-title">{course.title}</h5>
+                    <p className="card-text"><small className="text-body-secondary">{course.category}</small></p>
+                    <p className="card-text">{course.description}</p> 
+                </div>
+            </div>
+        </div>
     </div>
+
+    </>
   )
 }
 

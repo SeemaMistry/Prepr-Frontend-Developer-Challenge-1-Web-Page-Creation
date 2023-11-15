ae304cafe8d274a051e4cee99f217b93b7832a52
SeemaMistry <seema.m.1996@gmail.com>
2023-11-15 18:39:18 -0500

    feat: set Logo Icon based on course category type

--- prepr-homepage/src/components/CourseCard.js
+++ prepr-homepage/src/components/CourseCard.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import courseImg from '../assets/preprLogo.png'
+import labLogo from '../assets/lab_icon.svg'
+import projectLogo from '../assets/project_icon.svg'
+import challengeLogo from '../assets/challenge_icon.svg'
 
 const CourseCard = ({course}) => {
     // Course Object values
@@ -10,6 +13,8 @@ const CourseCard = ({course}) => {
     // description: 'Welcome to the CARE Lab, a space to collaborate and communicate ideas, resources, best practices, and awareness to help manage stress, relieve anxiety, and carry our collective burdens together. Participants are invited to share stories and resources while creating a safe space for everyone to share and learn from each other. Contributors are invited to host events and webinars that can be made ac...',
     // tags: ['communication', 'mindfullness', 'writing']
 
+    // set logoIcon by category type
+    const logoIcon = course.category === 'labs' ? labLogo : course.category === 'challenges' ? challengeLogo : projectLogo
   return (
     <>
     <div className="card mb-3" style={{maxWidth: '100%'}}>
@@ -20,7 +25,10 @@ const CourseCard = ({course}) => {
             <div className="col-md-8">
                 <div className="card-body">
                     <h5 className="card-title">{course.title}</h5>
-                    <p className="card-text"><small className="text-body-secondary">{course.category}</small></p>
+                    <p className="card-text">
+                        <img src={logoIcon} className="img-fluid rounded-start"/>
+                        <small className="text-body-secondary">{course.category}</small>
+                    </p>
                     <p className="card-text">{course.description}</p> 
                 </div>
             </div>

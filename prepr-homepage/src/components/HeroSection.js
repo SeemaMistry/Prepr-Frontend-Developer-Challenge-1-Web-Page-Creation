e8a58c39174d5f952c160c12058133c12729f11d
SeemaMistry <seema.m.1996@gmail.com>
2023-11-14 21:58:13 -0500

    styles: wrap Hero Section content in div with padding of 10rem to center text in gradient box

--- prepr-homepage/src/components/HeroSection.js
+++ prepr-homepage/src/components/HeroSection.js
@@ -3,7 +3,10 @@ import React from 'react'
 const HeroSection = () => {
   return (
     <div className='text-center' style={{backgroundImage: 'linear-gradient(to right, lightcyan, lightblue)', height: '30rem'}}>
-        <h1>Hero Section</h1>
+        <div style={{padding:'10rem'}}>
+            <h1>Hero Section</h1>
+            <p>A description of what this site is</p>
+        </div>
     </div>
   )
 }

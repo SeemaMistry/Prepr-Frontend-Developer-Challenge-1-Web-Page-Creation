f3d8c96b324cb0f1dca6cfb2515db3f68b4de742
SeemaMistry <seema.m.1996@gmail.com>
2023-11-14 22:43:13 -0500

    feat: initalize course content section component

--- prepr-homepage/src/App.js
+++ prepr-homepage/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
+import CoursesSection from "./components/CoursesSection";
 
 function App() {
   return (
     <>
     <Navbar />
     <HeroSection/>
+    <CoursesSection />
     <div className="m-5">placeholder div for testing scroll</div>
     <div className="m-5">placeholder div for testing scroll</div>
     <div className="m-5">placeholder div for testing scroll</div>

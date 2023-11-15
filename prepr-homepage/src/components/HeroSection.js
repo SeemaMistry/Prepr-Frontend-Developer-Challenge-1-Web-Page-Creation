712a99eb37221d36bb6e28094e4312ae2dbb9601
SeemaMistry <seema.m.1996@gmail.com>
2023-11-15 18:31:03 -0500

    fix: change class attributes to className

--- prepr-homepage/src/components/HeroSection.js
+++ prepr-homepage/src/components/HeroSection.js
@@ -13,11 +13,11 @@ const HeroSection = () => {
                 We’ll recommend content based on your interests we’ve listed here.<br></br>
                 Feel free to add or remove topics to customize your experience!
             </p>
-            <div class="input-group mb-5 mt-5">
-                <span class="input-group-text"><i class="bi bi-search"></i></span>
-                <div class="form-floating">
-                    <input type="text" class="form-control" id="floatingInputGroup2" placeholder="Username" required/>
-                    <label for="floatingInputGroup2">Search skills</label>
+            <div className="input-group mb-5 mt-5">
+                <span className="input-group-text"><i className="bi bi-search"></i></span>
+                <div className="form-floating">
+                    <input type="text" className="form-control" id="floatingInputGroup2" placeholder="" required/>
+                    <label htmlFor="floatingInputGroup2">Search skills</label>
                 </div>
             </div>
             <h2 className='mt-3 mb-5 fw-semibold'>Click an interest to narrow your <br></br>recommendations.</h2>

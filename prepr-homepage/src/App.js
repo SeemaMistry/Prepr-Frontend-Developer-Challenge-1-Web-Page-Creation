1643a8465d9b92a4e587c8f59a4be6cab5e95049
SeemaMistry <seema.m.1996@gmail.com>
2023-11-14 22:36:15 -0500

    test: add placeholder divs to App.js to allow testing for scrolling down page

--- prepr-homepage/src/App.js
+++ prepr-homepage/src/App.js
@@ -7,6 +7,12 @@ function App() {
     <>
     <Navbar />
     <HeroSection/>
+    <div className="m-5">placeholder div for testing scroll</div>
+    <div className="m-5">placeholder div for testing scroll</div>
+    <div className="m-5">placeholder div for testing scroll</div>
+    <div className="m-5">placeholder div for testing scroll</div>
+    <div className="m-5">placeholder div for testing scroll</div>
+
     </>
   );
 }

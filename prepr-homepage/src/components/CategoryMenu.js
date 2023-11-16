28eb37e4c50427f313bd94da46761d9272e0bd9c
SeemaMistry <seema.m.1996@gmail.com>
2023-11-16 00:02:40 -0500

    styles: remove button blue-outline when clicked and add shadow to menu

--- prepr-homepage/src/components/CategoryMenu.js
+++ prepr-homepage/src/components/CategoryMenu.js
@@ -5,14 +5,14 @@ const CategoryMenu = ({selectedCategory, onChange}) => {
         onChange(e.target.id)
     }
 
-    const baseBtnClasses = 'btn flex-fill btn-lg'
+    const baseBtnClasses = 'btn flex-fill btn-lg shadow-none'
 
   return (
-    <div className='d-flex text-center my-3' >
+    <div className='d-flex text-center my-3 shadow' >
         <button className={`${baseBtnClasses} rounded-end-0  ${selectedCategory === 'labs' ? 'btn-primary' : 'bg-light' }`} id='labs' onClick={setCatergoryHandler}>
             Labs
         </button>
-        <button className={`${baseBtnClasses} rounded-0 ${selectedCategory === 'challenges' ? 'btn-primary' : 'bg-light' }`} id='challenges' onClick={setCatergoryHandler}>
+        <button  className={`${baseBtnClasses} rounded-0 ${selectedCategory === 'challenges' ? 'btn-primary' : 'bg-light' }`} id='challenges' onClick={setCatergoryHandler}>
             Challenges
         </button>
         <button className={`${baseBtnClasses} rounded-0 bg-light`}>

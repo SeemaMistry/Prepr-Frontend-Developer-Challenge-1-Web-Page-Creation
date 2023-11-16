05284aec14e66b82d9eb0430cd5719ad1bb2ec1a
SeemaMistry <seema.m.1996@gmail.com>
2023-11-15 20:06:43 -0500

    styles: spread course category menu buttons across full length of container with d-flex and flex-fill

--- prepr-homepage/src/components/CoursesSection.js
+++ prepr-homepage/src/components/CoursesSection.js
@@ -13,11 +13,11 @@ const CoursesSection = ({filteredCourses, onChange}) => {
   
     return (
     <div className='container'>
-        <div className='text-center'>
-            <button className={`btn btn-lg ${selectedCategory === 'labs' ? 'btn-primary' : 'bg-light' }`} id='labs' onClick={setCatergoryHandler}>Labs</button>
-            <button className={`btn btn-lg ${selectedCategory === 'challenges' ? 'btn-primary' : 'bg-light' }`} id='challenges' onClick={setCatergoryHandler}>Challenges</button>
-            <button className='btn btn-lg bg-light'>Resources</button>
-            <button className={`btn btn-lg ${selectedCategory === 'projects' ? 'btn-primary' : 'bg-light' }`} id='projects' onClick={setCatergoryHandler}>Projects</button>
+        <div className='d-flex text-center my-3' >
+            <button className={`btn flex-fill btn-lg ${selectedCategory === 'labs' ? 'btn-primary' : 'bg-light' }`} id='labs' onClick={setCatergoryHandler}>Labs</button>
+            <button className={`btn flex-fill btn-lg ${selectedCategory === 'challenges' ? 'btn-primary' : 'bg-light' }`} id='challenges' onClick={setCatergoryHandler}>Challenges</button>
+            <button className='btn flex-fill btn-lg bg-light'>Resources</button>
+            <button className={`btn flex-fill btn-lg ${selectedCategory === 'projects' ? 'btn-primary' : 'bg-light' }`} id='projects' onClick={setCatergoryHandler}>Projects</button>
         </div>
         <div className='row'>
             <div className="col-2 mx-5 p-10">

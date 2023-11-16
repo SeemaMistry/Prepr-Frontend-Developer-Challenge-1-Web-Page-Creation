a9082ab4c8b0355996ceb1b9bde78203f8e430be
SeemaMistry <seema.m.1996@gmail.com>
2023-11-15 20:31:28 -0500

    chore: remove unused attribute from CourseSection

--- prepr-homepage/src/App.js
+++ prepr-homepage/src/App.js
@@ -20,7 +20,7 @@ function App() {
     <div style={{backgroundColor: '#f3f7fc'}}>
       <Navbar />
       <HeroSection/>
-      <CoursesSection state={selectedContent} onChange={onChangeHandler} filteredCourses={filteredCourses}/>
+      <CoursesSection onChange={onChangeHandler} filteredCourses={filteredCourses}/>
     </div>
   );
 }

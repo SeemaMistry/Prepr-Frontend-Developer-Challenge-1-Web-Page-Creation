b7bd7b74edd3cc1026536ed90304d47d7bf61e29
SeemaMistry <seema.m.1996@gmail.com>
2023-11-15 22:26:51 -0500

    styles: round corners of only left-most and right-most buttons in CategoryMenu

--- prepr-homepage/src/components/CategoryMenu.js
+++ prepr-homepage/src/components/CategoryMenu.js
@@ -7,16 +7,16 @@ const CategoryMenu = ({selectedCategory, onChange}) => {
     }
   return (
     <div className='d-flex text-center my-3' >
-        <button className={`btn flex-fill btn-lg ${selectedCategory === 'labs' ? 'btn-primary' : 'bg-light' }`} id='labs' onClick={setCatergoryHandler}>
+        <button className={`btn flex-fill btn-lg rounded-end-0  ${selectedCategory === 'labs' ? 'btn-primary' : 'bg-light' }`} id='labs' onClick={setCatergoryHandler}>
             Labs
         </button>
-        <button className={`btn flex-fill btn-lg ${selectedCategory === 'challenges' ? 'btn-primary' : 'bg-light' }`} id='challenges' onClick={setCatergoryHandler}>
+        <button className={`btn flex-fill btn-lg rounded-0 ${selectedCategory === 'challenges' ? 'btn-primary' : 'bg-light' }`} id='challenges' onClick={setCatergoryHandler}>
             Challenges
         </button>
-        <button className='btn flex-fill btn-lg bg-light'>
+        <button className='btn flex-fill btn-lg rounded-0 bg-light'>
             Resources
         </button>
-        <button className={`btn flex-fill btn-lg ${selectedCategory === 'projects' ? 'btn-primary' : 'bg-light' }`} id='projects' onClick={setCatergoryHandler}>
+        <button className={`btn flex-fill btn-lg rounded-start-0 ${selectedCategory === 'projects' ? 'btn-primary' : 'bg-light' }`} id='projects' onClick={setCatergoryHandler}>
             Projects
         </button>
     </div>

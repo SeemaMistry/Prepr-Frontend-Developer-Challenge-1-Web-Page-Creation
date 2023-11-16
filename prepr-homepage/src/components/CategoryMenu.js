73ae32d14adfeafb9b7d2da640681b610c5422c5
SeemaMistry <seema.m.1996@gmail.com>
2023-11-15 22:30:56 -0500

    refactor: extract common button classes into constant value

--- prepr-homepage/src/components/CategoryMenu.js
+++ prepr-homepage/src/components/CategoryMenu.js
@@ -3,20 +3,22 @@ import React from 'react'
 const CategoryMenu = ({selectedCategory, onChange}) => {
     const setCatergoryHandler = (e) => {
         onChange(e.target.id)
-
     }
+
+    const baseBtnClasses = 'btn flex-fill btn-lg'
+
   return (
     <div className='d-flex text-center my-3' >
-        <button className={`btn flex-fill btn-lg rounded-end-0  ${selectedCategory === 'labs' ? 'btn-primary' : 'bg-light' }`} id='labs' onClick={setCatergoryHandler}>
+        <button className={`${baseBtnClasses} rounded-end-0  ${selectedCategory === 'labs' ? 'btn-primary' : 'bg-light' }`} id='labs' onClick={setCatergoryHandler}>
             Labs
         </button>
-        <button className={`btn flex-fill btn-lg rounded-0 ${selectedCategory === 'challenges' ? 'btn-primary' : 'bg-light' }`} id='challenges' onClick={setCatergoryHandler}>
+        <button className={`${baseBtnClasses} rounded-0 ${selectedCategory === 'challenges' ? 'btn-primary' : 'bg-light' }`} id='challenges' onClick={setCatergoryHandler}>
             Challenges
         </button>
-        <button className='btn flex-fill btn-lg rounded-0 bg-light'>
+        <button className={`${baseBtnClasses} rounded-0 bg-light`}>
             Resources
         </button>
-        <button className={`btn flex-fill btn-lg rounded-start-0 ${selectedCategory === 'projects' ? 'btn-primary' : 'bg-light' }`} id='projects' onClick={setCatergoryHandler}>
+        <button className={`${baseBtnClasses} rounded-start-0 ${selectedCategory === 'projects' ? 'btn-primary' : 'bg-light' }`} id='projects' onClick={setCatergoryHandler}>
             Projects
         </button>
     </div>

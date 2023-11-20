363dc92b48fe20a055fb0646c145f300f34d1fd6
SeemaMistry <seema.m.1996@gmail.com>
2023-11-20 11:33:37 -0500

    feat: conditionally apply dark mode to card body, follow button, Tags

--- prepr-homepage/src/components/CourseCard.js
+++ prepr-homepage/src/components/CourseCard.js
@@ -4,14 +4,16 @@ import projectLogo from '../assets/project_icon.svg'
 import challengeLogo from '../assets/challenge_icon.svg'
 import Tag from './Tag'
 
-const CourseCard = ({course}) => {
+const CourseCard = ({course, activateDarkMode}) => {
     // set logoIcon by category type
     const logoIcon = course.category === 'labs' ? labLogo : course.category === 'challenges' ? challengeLogo : projectLogo
     const categoryTitle = course.category === 'labs' ? 'Public Lab' : course.category === 'challenges' ? "Pubic Challenge" : "Public"
+    const cardDarkMode = activateDarkMode ? 'text-bg-secondary' : ''
+    const btnFollowDarkMode = activateDarkMode ? 'btn-light' : ''
 
   return (
     <>
-    <div className="card mb-3" style={{maxWidth: '100%'}}>
+    <div className={`card mb-3 ${cardDarkMode}`} style={{maxWidth: '100%'}}>
         <div className="row g-0">
             <div className="col-md-4">
                 <div className='p-3'>
@@ -28,7 +30,7 @@ const CourseCard = ({course}) => {
                 <div className="card-body">
                     <div className='d-flex justify-content-between'>
                         <h5 className="card-title">{course.title}</h5>
-                        {course.category === 'labs' ? <button className='btn btn-primary'>+ Follow</button> : ''}
+                        {course.category === 'labs' ? <button className={`btn btn-primary ${btnFollowDarkMode}`}>+ Follow</button> : ''}
                     </div>
                     <p className="card-text">
                         <img src={logoIcon} className="img-fluid rounded-start" alt="category logo"/>
@@ -36,7 +38,7 @@ const CourseCard = ({course}) => {
                     </p>
                     <p className="card-text">{course.description}</p> 
                     <p>
-                        {course.tags.map(tag => <Tag key={`${course.id}-${tag}`} title={tag}/>)}
+                        {course.tags.map(tag => <Tag key={`${course.id}-${tag}`} title={tag} activateDarkMode={activateDarkMode}/>)}
                     </p>
                 </div>
             </div>

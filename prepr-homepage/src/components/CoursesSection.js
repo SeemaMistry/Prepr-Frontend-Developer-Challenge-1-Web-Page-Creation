ea07cc8ff6c95899ee3122dc56a0ea6f2c64fbe2
SeemaMistry <seema.m.1996@gmail.com>
2023-11-26 18:52:41 -0500

    feat: dynamically render sideMenuOne only for labs and challenges and pass state to dynamically render menu title and subheadings

--- prepr-homepage/src/components/CoursesSection.js
+++ prepr-homepage/src/components/CoursesSection.js
@@ -8,28 +8,14 @@ import SideMenuTwo from './SideMenuTwo'
 const CoursesSection = ({state, filteredCourses, onChange, activateDarkMode}) => {
     const setCatergoryHandler = (id) => onChange(id)
 
-    return (
-    // <div className='container-xxl'>
-    //    <CategoryMenu onChange={setCatergoryHandler} selectedCategory={state} activateDarkMode={activateDarkMode}/>
-    //     <div className='row'>
-    //         <div className="col-2 mx-5 p-0">
-    //             <SideMenuOne activateDarkMode={activateDarkMode}/>
-    //             <SideMenuTwo activateDarkMode={activateDarkMode}/>
-    //         </div>
-    //         <div className="col-9">
-    //             <FilterSection activateDarkMode={activateDarkMode}/>
-    //             <div className='my-3'>
-    //                 {filteredCourses.map(item => <CourseCard key={item.id} course={item} activateDarkMode={activateDarkMode}/>)}
-    //             </div>
-    //         </div>
-    //     </div>
-    // </div>
-    
+    return (   
     <div className='container-xxl'>
     <CategoryMenu onChange={setCatergoryHandler} selectedCategory={state} activateDarkMode={activateDarkMode}/>
      <div className='row'>
-         <div className={`col-md-2  mx-0 my-0 p-0`}>
-             <SideMenuOne activateDarkMode={activateDarkMode}/>
+         <div className={`col-md-2  mx-0 my-2 p-2`}>
+            {/* conditionally render sideMenuOne only for labs and challenges */}
+            {state === 'challenges' &&  <SideMenuOne activateDarkMode={activateDarkMode} selectedCategory={state}/> }
+            {state === 'labs' &&  <SideMenuOne activateDarkMode={activateDarkMode} selectedCategory={state}/> }
              <SideMenuTwo activateDarkMode={activateDarkMode}/>
          </div>
          <div className={`col-md-10 `}>
--- prepr-homepage/src/components/SideMenuOne.js
+++ prepr-homepage/src/components/SideMenuOne.js
@@ -1,16 +1,20 @@
 import React from 'react'
 
-const SideMenuOne = ({activateDarkMode}) => {
+const SideMenuOne = ({activateDarkMode, selectedCategory}) => {
     // apply dark mode styling
     const btnHeadingDarkMode = activateDarkMode ? 'btn-success text-dark' : 'btn-primary'
     const btnCategoryDarkMode = activateDarkMode ? 'btn-secondary' : 'btn-light'
 
     const baseBtnClasses = 'btn btn-lg text-start shadow-lg'
+
+    // set title and subheading
+    const title = selectedCategory === 'labs' ? 'Labs' : 'Challenges'
+    const subHeading = selectedCategory === 'labs' ? 'Lab Programs' : 'Challenge Path'
     
   return (
     <div className='d-grid mb-5'>
-        <button className={`${baseBtnClasses} ${btnHeadingDarkMode} rounded-bottom-0`}>Labs</button>
-        <button className={`${baseBtnClasses} ${btnCategoryDarkMode} rounded-top-0`}>Lab Programs</button>
+        <button className={`${baseBtnClasses} ${btnHeadingDarkMode} rounded-bottom-0`}>{title}</button>
+        <button className={`${baseBtnClasses} ${btnCategoryDarkMode} rounded-top-0`}>{subHeading}</button>
     </div>
   )
 }

d093043a20bbb923565b0044cbf96f53bcf68d6f
SeemaMistry <seema.m.1996@gmail.com>
2023-11-30 14:02:04 -0500

    feat: create button to open and close the accessabilityModal

--- prepr-homepage/src/App.js
+++ prepr-homepage/src/App.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from "react";
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
 import CoursesSection from "./components/CoursesSection";
+import AccessabilityModal from "./components/AccessabilityModal";
 
 // Dummy data for course cards
 import {ALL_CONTENT} from './data'
@@ -21,50 +22,18 @@ function App() {
   // filter courses by state
   const filteredCourses = ALL_CONTENT.filter(item => item.category === selectedContent)
 
-  // reset to default values 
-  const [activateDefaultSettings, setActivateResetSettings] = useState(false)
+  // open and close the accessability modal
+  const [modalActive, setModalActive] = useState(false)
 
-  // content scaling increase and decrease
-  const [scale, setScale] = useState(100)
-  const increaseScale = () => {
-    if (scale < 200) {
-      setScale(scale + 1)
-    }
-  }
-  const decreaseScale = () => {
-    if (scale > 0) {
-      setScale(scale - 1)
-    }
-  }
-
-  useEffect ( () => {
-    if(scale === 0){
-      setScale('none')
-    }
-    document.body.style.zoom = `${scale}%`
-
-  },[scale])
-
-  // activate reset
-  const defaultReset = () => setActivateResetSettings(true)
-
-  // reset scale to default 100
-  useEffect ( () => {
-    if(activateDefaultSettings){
-      setScale(100)
-    }
-    document.body.style.zoom = `${scale}%`
-    setActivateResetSettings(false) // reset to false!!
-  },[activateDefaultSettings])
+  const openModal = () => setModalActive(prevState => !prevState)
 
   return (
     <div 
     data-bs-theme={darkModeIsActivate ? 'dark' : ''} 
     style={{backgroundColor: `${bgColorDarkMode}`}}
     >
-      <button className="btn btn-warning btn-lg" onClick={defaultReset}>Reset</button>
-       <button className="btn btn-warning btn-lg" onClick={increaseScale}>Increase</button>
-      <button className="btn btn-warning btn-lg" onClick={decreaseScale}>decrease</button>
+      <button onClick={openModal}>Open Accessability Modal</button>
+      {modalActive && <AccessabilityModal/>}
       <Navbar activateDarkMode={darkModeIsActivate} onDarkMode={onDarkModeHandler}/>
       <HeroSection activateDarkMode={darkModeIsActivate}/>
       <CoursesSection onChange={onChangeHandler} filteredCourses={filteredCourses} state={selectedContent} activateDarkMode={darkModeIsActivate}/>

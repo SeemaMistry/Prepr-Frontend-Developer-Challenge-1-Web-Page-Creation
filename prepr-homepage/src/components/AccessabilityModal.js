156ed76f2f4dd96f514c90963d6899ed767b1da3
SeemaMistry <seema.m.1996@gmail.com>
2023-11-30 14:03:02 -0500

    refactor: extract scaling buttons (reset, increacse, decrease) to custom component AccessabilityModal

---
+++ prepr-homepage/src/components/AccessabilityModal.js
@@ -0,0 +1,49 @@
+import React, {useState, useEffect} from 'react'
+
+const AccessabilityModal = () => {
+    // reset to default values 
+    const [activateDefaultSettings, setActivateResetSettings] = useState(false)
+
+    // content scaling increase and decrease
+    const [scale, setScale] = useState(100)
+    const increaseScale = () => {
+        if (scale < 200) {
+        setScale(scale + 1)
+        }
+    }
+    const decreaseScale = () => {
+        if (scale > 0) {
+        setScale(scale - 1)
+        }
+    }
+
+    useEffect ( () => {
+        if(scale === 0){
+        setScale('none')
+        }
+        document.body.style.zoom = `${scale}%`
+
+    },[scale])
+
+    // activate reset
+    const defaultReset = () => setActivateResetSettings(true)
+
+    // reset scale to default 100
+    useEffect ( () => {
+        if(activateDefaultSettings){
+        setScale(100)
+        }
+        document.body.style.zoom = `${scale}%`
+        setActivateResetSettings(false) // reset to false!!
+    },[activateDefaultSettings])
+
+  return (
+    <div>
+        <button className="btn btn-warning btn-lg" onClick={defaultReset}>Reset</button>
+        <button className="btn btn-warning btn-lg" onClick={increaseScale}>Increase</button>
+        <button className="btn btn-warning btn-lg" onClick={decreaseScale}>decrease</button>
+    </div>
+  )
+}
+
+export default AccessabilityModal
